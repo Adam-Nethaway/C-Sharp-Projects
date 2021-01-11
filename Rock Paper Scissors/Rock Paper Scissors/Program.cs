1ff46621c4ac0fa21ea5de7478ad474188241d75
Adam Nethaway <76920701+chirpygorilla@users.noreply.github.com>
2021-01-11 01:16:08 -0600

    Beginning

---
+++ Rock Paper Scissors/Rock Paper Scissors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerInput, aiInput;
+            int aiChoice;
+            int cpuPoints = 0;
+            int playerPoints = 0;
+            
+            Random randomChoice = new Random();
+
+            aiChoice = randomChoice.Next(1, 4);
+
+            do
+            {
+
+            } while (cpuPoints < 3 && playerPoints < 3);
+        }
+    }
+}
