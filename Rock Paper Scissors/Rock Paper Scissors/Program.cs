02bc065ccc23011ed6231fa23be6cd79030b291b
Adam Nethaway <76920701+chirpygorilla@users.noreply.github.com>
2021-01-11 01:30:09 -0600

    Added Switch

--- Rock Paper Scissors/Rock Paper Scissors/Program.cs
+++ Rock Paper Scissors/Rock Paper Scissors/Program.cs
@@ -19,8 +19,51 @@ namespace Rock_Paper_Scissors
 
             aiChoice = randomChoice.Next(1, 4);
 
+            Console.WriteLine("Welcome to Rock Paper Scissors. First to 3 wins!");
+
             do
             {
+                Console.WriteLine("Rock, Paper, or Scissors? " + " Player " + playerPoints + " Computer " + cpuPoints);
+                string playerChoice = Console.ReadLine();
+
+                switch (aiChoice)
+                {
+                    case 1: //Rock
+                        Console.WriteLine("Computer chose rock");
+                        
+                        if (playerChoice == "rock")
+                        {
+                            Console.WriteLine("Draw\n");
+                        }
+                        else if (playerChoice == "paper")
+                        {
+                            Console.WriteLine("You win!\n");
+                            playerPoints++;
+                        }
+                        else if (playerChoice == "scissors")
+                        {
+                            Console.WriteLine("You lose!");
+                            cpuPoints++;
+                        }
+                        break;
+                    case 2: //Paper
+                        Console.WriteLine("Computer chose paper");
+
+                        if (playerChoice == "rock")
+                        {
+                            Console.WriteLine("You lose!\n");
+                            cpuPoints++;
+                        }
+                        else if (playerChoice == "paper")
+                        {
+                            Console.WriteLine("Draw!\n");
+                        }
+                        else if (playerChoice == "scissors")
+                        {
+                            Console.WriteLine("You win!\n");
+                            playerPoints++;
+                        }
+                }
 
             } while (cpuPoints < 3 && playerPoints < 3);
         }
