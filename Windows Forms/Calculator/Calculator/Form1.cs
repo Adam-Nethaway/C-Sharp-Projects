8f0d8d411ebcc4a504c3244e0b384ecd7388c05d
Adam Nethaway <76920701+chirpygorilla@users.noreply.github.com>
2021-01-22 14:50:30 -0600

    1.3
    Button Functionality

--- Windows Forms/Calculator/Calculator/Form1.cs
+++ Windows Forms/Calculator/Calculator/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Calculator
 {
- 
+
     public partial class Form1 : Form
     {
         string userInput = "";
@@ -22,6 +22,79 @@ namespace Calculator
         public Form1()
         {
             InitializeComponent();
+
+            this.KeyPreview = true;
+
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.NumPad1 || e.KeyCode == Keys.D1)
+            {
+                buttonOne.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad2 || e.KeyCode == Keys.D2)
+            {
+                buttonTwo.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad3 || e.KeyCode == Keys.D3)
+            {
+                buttonThree.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad4 || e.KeyCode == Keys.D4)
+            {
+                buttonFour.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad5 || e.KeyCode == Keys.D5)
+            {
+                buttonFive.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad6 || e.KeyCode == Keys.D6)
+            {
+                buttonSix.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad7 || e.KeyCode == Keys.D7)
+            {
+                buttonSeven.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad8 || e.KeyCode == Keys.D8)
+            {
+                buttonEight.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad9 || e.KeyCode == Keys.D9)
+            {
+                buttonNine.PerformClick();
+            }
+            if (e.KeyCode == Keys.NumPad0 || e.KeyCode == Keys.D0)
+            {
+                buttonZero.PerformClick();
+            }
+            if (e.KeyCode == Keys.Decimal)
+            {
+                buttonDecimal.PerformClick();
+            }
+            if (e.KeyCode == Keys.Add || e.KeyCode == Keys.Oemplus)
+            {
+                buttonPlus.PerformClick();
+            }
+            if (e.KeyCode == Keys.Subtract || e.KeyCode == Keys.OemMinus)
+            {
+                buttonMinus.PerformClick();
+            }
+            if (e.KeyCode == Keys.Divide)
+            {
+                buttonDivide.PerformClick();
+            }
+            if (e.KeyCode == Keys.Multiply)
+            {
+                buttonMultiply.PerformClick();
+            }
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.Focus();
+                buttonEquals.PerformClick();
+            }
         }
 
         private void buttonOne_Click(object sender, EventArgs e)
@@ -29,6 +102,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "1";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonTwo_Click(object sender, EventArgs e)
@@ -36,6 +110,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "2";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonThree_Click(object sender, EventArgs e)
@@ -43,6 +118,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "3";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonFour_Click(object sender, EventArgs e)
@@ -50,6 +126,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "4";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonFive_Click(object sender, EventArgs e)
@@ -57,6 +134,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "5";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonSix_Click(object sender, EventArgs e)
@@ -64,6 +142,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "6";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonSeven_Click(object sender, EventArgs e)
@@ -71,6 +150,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "7";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonEight_Click(object sender, EventArgs e)
@@ -78,6 +158,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "8";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonNine_Click(object sender, EventArgs e)
@@ -85,6 +166,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "9";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonZero_Click(object sender, EventArgs e)
@@ -92,6 +174,7 @@ namespace Calculator
             textBox1.Text = "";
             userInput += "0";
             textBox1.Text += userInput;
+            buttonEquals.Focus();
         }
 
         private void buttonDecimal_Click(object sender, EventArgs e)
@@ -157,37 +240,43 @@ namespace Calculator
             result = 0.0;
             textBox1.Text = "0";
         }
-
         private void buttonEquals_Click(object sender, EventArgs e)
         {
-            secondInput = userInput;
-
-            double firstNumber, secondNumber;
-
-            firstNumber = Convert.ToDouble(firstInput);
-            secondNumber = Convert.ToDouble(secondInput);
-
-            switch (operation)
+            try
             {
-                case '+':
-                    result = firstNumber + secondNumber;
-                    textBox1.Text = result.ToString();
-                    break;
-                case '-':
-                    result = firstNumber - secondNumber;
-                    textBox1.Text = result.ToString();
-                    break;
-                case '*':
-                    result = firstNumber * secondNumber;
-                    textBox1.Text = result.ToString();
-                    break;
-                case '/':
-                    result = firstNumber / secondNumber;
-                    textBox1.Text = result.ToString();
-                    break;
-            }
+                secondInput = userInput;
 
-            userInput = result.ToString();
+                double firstNumber, secondNumber;
+
+                firstNumber = Convert.ToDouble(firstInput);
+                secondNumber = Convert.ToDouble(secondInput);
+
+                switch (operation)
+                {
+                    case '+':
+                        result = firstNumber + secondNumber;
+                        textBox1.Text = result.ToString();
+                        break;
+                    case '-':
+                        result = firstNumber - secondNumber;
+                        textBox1.Text = result.ToString();
+                        break;
+                    case '*':
+                        result = firstNumber * secondNumber;
+                        textBox1.Text = result.ToString();
+                        break;
+                    case '/':
+                        result = firstNumber / secondNumber;
+                        textBox1.Text = result.ToString();
+                        break;
+                }
+
+                userInput = result.ToString();
+            }
+            catch (FormatException)
+            {
+                textBox1.Text = "Error";
+            }
         }
     }
 }
